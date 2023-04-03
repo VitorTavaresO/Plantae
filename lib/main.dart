f430ac827a0ab41daa9b36460b1931eaa432d99f
VitorTavaresO <vitortavares.o55@gmail.com>
2023-04-03 20:44:08 -0300

    Plano de Fundo Testes

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Plantae',
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Plantae'),
@@ -24,7 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
@@ -32,45 +30,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-
-              colors: [Color.fromRGBO(0,173, 132, 1), 
-              Color.fromRGBO(0,173, 132, 1),
-              Color.fromRGBO(0,173, 132, 1),  
-              Color.fromRGBO(28,199,0,1),   
-              Color.fromRGBO(24,174,0,1),  
-              Color.fromRGBO(24,174,0,1)]),
-
+        gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromRGBO(0, 173, 132, 1),
+              Color.fromRGBO(0, 173, 132, 1),
+              Color.fromRGBO(0, 173, 132, 1),
+              Color.fromRGBO(28, 199, 0, 1),
+              Color.fromRGBO(24, 174, 0, 1),
+              Color.fromRGBO(24, 174, 0, 1)
+            ]),
+      ),
       child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Column (
-             mainAxisAlignment : MainAxisAlignment.center,
-            children: [
-              Align(
-            alignment: Alignment.bottomCenter,  
-            child: Image.asset(
-              'assets/images/PlantaE_Logo(w).png',
-              height: 200,
-              width: 200,
-              ), 
+        backgroundColor: Colors.transparent,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Image.asset(
+                'assets/images/PlantaE_Logo(w).png',
+                height: 200,
+                width: 200,
+              ),
             ),
-             const Text(
-            'PLANTAE', 
-            style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            color: Colors.white, ), 
+            const Text(
+              'PLANTAE',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
             ),
-            ],
-             ), 
-          ),
+          ],
+        ),
+      ),
     );
   }
 }
