7bc9441f18fc8e81dc45da4e9b7caefbc99f1f87
Argustor <renatoplatz11@outlook.com>
2023-04-10 22:43:35 -0300

    começando o desenvolvimento da tela de cadastro

---
+++ lib/registerScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreen();
+}
+
+class _RegisterScreen extends State<Register
